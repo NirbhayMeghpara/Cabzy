75a012944a79ba8f824eaa11010a4c9fdbea50d9
Nirbhay_Elluminati <nirbhay.elluminati@gmail.com>
2023-10-23 18:26:18 +0530

    Socker Commit

--- src/app/component/confirmed-ride/assign-dialog/assign-dialog.component.ts
+++ src/app/component/confirmed-ride/assign-dialog/assign-dialog.component.ts
@@ -1,21 +1,25 @@
 import { Driver } from "./../../driver-list/driver-list.component"
 import { DriverService } from "src/app/services/driver/driver.service"
-import { Component, Inject, OnInit } from "@angular/core"
-import { MAT_DIALOG_DATA } from "@angular/material/dialog"
+import { Component, Inject, OnDestroy, OnInit } from "@angular/core"
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog"
 import { ToastService } from "src/app/services/toast.service"
+import { SocketService } from "src/app/services/socket/socket.service"
 
 @Component({
   selector: "app-assign-dialog",
   templateUrl: "./assign-dialog.component.html",
   styleUrls: ["./assign-dialog.component.scss"],
 })
-export class AssignDialogComponent implements OnInit {
+export class AssignDialogComponent implements OnInit, OnDestroy {
   drivers: Driver[] = []
+  selectedDriver!: Driver
 
   constructor(
+    private dialogRef: MatDialogRef<AssignDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public ride: any,
     private driverService: DriverService,
-    private toast: ToastService
+    private toast: ToastService,
+    private socketService: SocketService
   ) {}
 
   ngOnInit(): void {
@@ -27,7 +31,42 @@ export class AssignDialogComponent implements OnInit {
         this.toast.error(error.error.error, "Error")
       },
     })
+    this.socketService.initializeSocket()
   }
 
-  assignToSelectedDriver() {}
+  onDriverSelected(index: number) {
+    this.selectedDriver = this.drivers[index]
+  }
+
+  assignToSelectedDriver() {
+    if (this.selectedDriver) {
+      this.socketService.emit(
+        "assignToSelectedDriver",
+        { ride: this.ride, driver: this.selectedDriver },
+        (error, message) => {
+          if (error) {
+            console.error("Error:", error)
+            this.dialogRef.close({ error })
+          } else {
+            console.log("Message:", message)
+            this.dialogRef.close({ updatedRide: message })
+          }
+        }
+      )
+    } else {
+      this.toast.info("Please select a driver", "Info")
+    }
+  }
+
+  assignToNearestDriver() {
+    if (this.selectedDriver) {
+      this.socketService.emit("assignToNearestDriver", this.ride)
+    } else {
+      this.toast.info("Please select a driver", "Info")
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.socketService.disconnectSocket()
+  }
 }
