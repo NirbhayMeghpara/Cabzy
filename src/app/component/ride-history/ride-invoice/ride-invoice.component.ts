43384cf6196e4419f41f2c810dcdcdc6e8a66092
Nirbhay Meghpara <nirbhaymeghpara123@gmail.com>
2023-11-06 00:40:48 +0530

    Styling of invoice remaining

--- src/app/component/ride-history/ride-invoice/ride-invoice.component.ts
+++ src/app/component/ride-history/ride-invoice/ride-invoice.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-ride-invoice',
   templateUrl: './ride-invoice.component.html',
   styleUrls: ['./ride-invoice.component.scss']
 })
-export class RideInvoiceComponent {
+export class RideInvoiceComponent implements OnInit{
+
+  stopAddress: string[] = []
+
+  constructor(@Inject(MAT_DIALOG_DATA) public ride: any) {}
+
+  ngOnInit(): void {
+    this.getStopAddress()
+  }
+
+  getStopAddress() {
+    this.ride.stops.forEach((stop: any) => {
+      const geocoder = new google.maps.Geocoder()
+
+      geocoder.geocode({ location: stop }, (results, status) => {
+        if (status === "OK" && results) {
+          if (results[0]) {
+            this.stopAddress.push(results[0].formatted_address)
+          }
+        } else {
+          console.error("Geocoder failed due to: " + status)
+        }
+      })
+    })
+  }
 
 }
